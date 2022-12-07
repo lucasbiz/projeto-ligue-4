6e6913d70a0ebf5ea6e2d4d4901df402c219d308
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-12-07 02:31:37 -0300

    Update connectFour.py

--- connectFour.py
+++ connectFour.py
@@ -63,7 +63,14 @@ def analiseLinhaColuna(jogador,matriz):
                 elif rep == 4:
                     return True
                 else:
-                    rep = 0        
+                    rep = 0   
+            col +=1
+
+    #analise de diagonais jogador 1
+        diag = 0
+
+
+
 
     else:
     #analise de linhas jogador 2
@@ -86,6 +93,8 @@ def analiseLinhaColuna(jogador,matriz):
                     return True
                 else:
                     rep = 0 
+            col +=1     
+
     return False
 #=================== FUNCOES PARA O JOGO =================#
 
@@ -109,81 +118,79 @@ while jogarNovamente:
     winner = False
 
     if menu == '1':  
-        while not winner:
+        tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
+        largura = int(tabuleiro[0])
+        altura = int(tabuleiro[1])
+
+        #verificacao de dimensoes do tabuleiro
+        while largura > 10 or largura<=0 or altura > 10 or altura<=0:
+            print('Medidas inválidas de tabuleiro, por favor digite novamente!')
             tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
-            largura = int(tabuleiro[0])
-            altura = int(tabuleiro[1])
+        matrizGame = []
+        qtdLinhas = 0
 
-            #verificacao de dimensoes do tabuleiro
-            while largura > 10 or largura<=0 or altura > 10 or altura<=0:
-                print('Medidas inválidas de tabuleiro, por favor digite novamente!')
-                tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
-            matrizGame = []
-            qtdLinhas = 0
-
-            #criacao do tabuleiro(matriz)
-            while qtdLinhas < altura:
-                qtdCol = 0
-                linha = []
-                while qtdCol < largura:
-                    linha.append(0)
-                    qtdCol+=1
-                matrizGame.append(linha)
-                qtdLinhas+=1
+        #criacao do tabuleiro(matriz)
+        while qtdLinhas < altura:
+            qtdCol = 0
+            linha = []
+            while qtdCol < largura:
+                linha.append(0)
+                qtdCol+=1
+            matrizGame.append(linha)
+            qtdLinhas+=1
 
 
-            print('-'*20)    
-            print('Este é o tabuleiro da sua partida! Que vença o melhor! ')  
-            mostraMatriz(matrizGame)
+        print('-'*20)    
+        print('Este é o tabuleiro da sua partida! Que vença o melhor! ')  
+        mostraMatriz(matrizGame)
 
-            # Decidindo qual jogador começa
+        # Decidindo qual jogador começa
 
-            print('Para decidir qual jogador irá iniciar faremos um sorteio... ')
-            sorteio = random.randint(-1,1)
-            time.sleep(3)
-            print('E o jogador que iniciará será...')
-            time.sleep(3)
-            if sorteio == 0:
-                print('* Jogador 1! Você começa e ficará com as peças de número 1!')
-                vez = 0
+        print('Para decidir qual jogador irá iniciar faremos um sorteio... ')
+        sorteio = random.randint(-1,1)
+        time.sleep(3)
+        print('E o jogador que iniciará será...')
+        time.sleep(3)
+        if sorteio == 0:
+            print('* Jogador 1! Você começa e ficará com as peças de número 1!')
+            vez = 0
+        else:
+            print('* Jogador 2! Você começa e ficará com as peças de número 2!')
+            vez = 1
+
+        #iniciando o jogo
+        espacosVazios = altura * largura
+        while espacosVazios >= 0:
+            if vez%2==0:
+                jogada(1,matrizGame)
             else:
-                print('* Jogador 2! Você começa e ficará com as peças de número 2!')
-                vez = 1
-
-            #iniciando o jogo
-            espacosVazios = altura * largura
-            while espacosVazios >= 0:
-                if vez%2==0:
-                    jogada(1,matrizGame)
+                jogada(2,matrizGame)
+            #condicao mínima para uma vitoria é de pelo menos 7 jogadas (4 do vencedor e 3 do perdedor)
+            #inicia-se as verificacoes
+            if vez >= 7:
+                if vez % 2 ==0:
+                    if analiseLinhaColuna(1,matrizGame):
+                        print('==== Jogador 1 venceu! ====')
+                        break
                 else:
-                    jogada(2,matrizGame)
-                #condicao mínima para uma vitoria é de pelo menos 7 jogadas (4 do vencedor e 3 do perdedor)
-                #inicia-se as verificacoes
-                if vez >= 7:
-                    if vez%2 ==0:
-                        if analiseLinhaColuna(1,matrizGame):
-                            print('==== Jogador 1 venceu! ====')
-                            winner = True
-                    else:
-                        if analiseLinhaColuna(2,matrizGame):
-                            print('==== Jogador 2 venceu! ====')
-                            winner = True
-                vez+=1
-                espacosVazios-=1
+                    if analiseLinhaColuna(2,matrizGame):
+                        print('==== Jogador 2 venceu! ====')
+                        break
+            vez+=1
+            espacosVazios-=1
+        if espacosVazios == 0:
+            print('Nenhum dos jogadores chegaram na vitória e os espaços acabaram! EMPATE!')
     else:
         xxx = 0    
 
-
-
     #pergunta se o jogador deseja reiniciar o jogo                   
     jogarNovamente = input('Deseja jogar novamente?:\n 1 - SIM\n 2 - NAO\n ')
-    if jogarNovamente in '1simSIM':
+    if jogarNovamente in ['1','sim','SIM']:
         jogarNovamente = True
-    elif jogarNovamente in '2naoNAO':
+    elif jogarNovamente in ['2','nao','NAO']:
         jogarNovamente = False
     else:
-        print('Opção inválida, digite apenas 1, 2, SIM ou NAO')
-        jogarNovamente = input('Deseja jogar novamente?:\n 1 - SIM\n 2 - NAO\n ')
+        print('Opção inválida, jogo finalizado.')
 
 
 
