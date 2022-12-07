2cfc0b42bb8cd9e6145e17ccb07611873662a37c
Lucas Biz Teixeira <lucaasbizteixeira@gmail.com>
2022-12-07 03:34:46 -0300

    Update connectFour.py

--- connectFour.py
+++ connectFour.py
@@ -18,23 +18,21 @@ def mostraMatriz(matriz):
 
 #faz a jogada 
 def jogada(jogador,matriz):
-
     linhaJogada = len(matriz) - 1
-
-    #jogador 1
-    if jogador == 1: 
-        coluna = int(input('Jogador 1, escolha a coluna que deseja colocar a peça!: '))
-        while linhaJogada >= 0:
-            if matriz[linhaJogada][coluna] == 0:
-                matriz[linhaJogada][coluna] = 1
-                break
-            linhaJogada -=1
-    #jogador 2
-    else:
-        coluna = int(input('Jogador 2, escolha a coluna que deseja colocar a peça!: '))
+    if jogador == 3:
+        time.sleep(2)
+        coluna = random.randint(-1,len(matriz)-1)
+        print(f'Jogador 2 escolhe a coluna {coluna}')
         while linhaJogada >= 0:
             if matriz[linhaJogada][coluna] == 0:
                 matriz[linhaJogada][coluna] = 2
+                break
+            linhaJogada -=1
+    else:
+        coluna = int(input(f'Jogador {jogador}, escolha a coluna que deseja colocar a peça!: '))
+        while linhaJogada >= 0:
+            if matriz[linhaJogada][coluna] == 0:
+                matriz[linhaJogada][coluna] = jogador
                 break
             linhaJogada -=1
     mostraMatriz(matriz)
@@ -43,59 +41,105 @@ def jogada(jogador,matriz):
 
 #funcoes para fazer as analises de possiveis jogadas vencedoras
 def analiseLinhaColuna(jogador,matriz):
-    if jogador == 1:
-    #analise de linhas jogador 1
+    #analise de linhas 
+    for linha in matriz:
+        rep = 0
+        for el in linha:
+            if el == jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0
+#analise de colunas 
+    col = 0
+    rep = 0
+    while col < len(matriz[0]):
         for linha in matriz:
-            rep = 0
-            for el in linha:
-                if el == 1:
-                    rep +=1
-                elif rep == 4:
-                    return True
-                else:
-                    rep = 0
-    #analise de colunas jogador 1
-        col = 0
-        while col < len(matriz[0]):
-            for linha in matriz:
-                if linha[col] == 1:
-                    rep +=1
-                elif rep == 4:
-                    return True
-                else:
-                    rep = 0   
-            col +=1
-
-    #analise de diagonais jogador 1
-        diag = 0
-
-
-
-
-    else:
-    #analise de linhas jogador 2
-        for linha in matriz:
-            rep = 0
-            for el in linha:
-                if el == 2:
-                    rep +=1
-                elif rep == 4:
-                    return True
-                else:
-                    rep = 0
-    #analise de colunas jogador 2
-        col = 0
-        while col < len(matriz[0]):
-            for linha in matriz:
-                if linha[col] == 2:
-                    rep +=1
-                elif rep == 4:
-                    return True
-                else:
-                    rep = 0 
-            col +=1     
-
+            if linha[col] == jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0   
+        col +=1
     return False
+
+
+
+#funcao analise diagonais
+def analiseDiag(jogador,matriz):
+
+  #analisa diagonal principal para baixo (sem contar a principal)
+    cont = 1
+    rep = 0
+    while cont <len(matriz):
+        col = 0
+        linhaIni = cont
+        while linhaIni < len(matriz):
+            if matriz[linhaIni][col] ==jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0
+            col +=1
+            linhaIni += 1
+        cont +=1
+
+    #analisa diagonal principal para cima (contando com a principal)
+    cont = 0
+    rep = 0
+    while cont < len(matriz):
+        col = len(matriz) - 1
+        linhaIni = cont
+        while linhaIni >= 0:
+            if matriz[linhaIni][col] ==jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0
+            col -=1
+            linhaIni -= 1
+        cont +=1
+
+    #analisa diagonal secundária (da secunda para baixo contando com ela)
+    cont = len(matriz) - 1
+    rep = 0
+    while cont >= 0:
+        col = 0
+        linhaIni = cont
+        while linhaIni >= 0:
+            if matriz[linhaIni][col] ==jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0 
+            col +=1
+            linhaIni -= 1
+        cont -=1
+
+
+    #analisa diagonal secundária (da secunda para cima sem contar com ela)
+    cont = 0
+    rep = 0
+    while cont < len(matriz)-1:
+        col = 0
+        linhaIni = cont
+        while linhaIni >= 0:
+            if matriz[linhaIni][col] ==jogador:
+                rep +=1
+            elif rep == 4:
+                return True
+            else:
+                rep = 0
+            col +=1
+            linhaIni -= 1
+        cont +=1
+    return False
+
 #=================== FUNCOES PARA O JOGO =================#
 
 
@@ -115,15 +159,14 @@ while jogarNovamente:
     while menu not in '12':
         menu = input("Digite apenas 1 ou 2 para selecionar as opções!\n")
 
-    winner = False
-
+#========================= JOGADOR X JOGADOR =====================#
     if menu == '1':  
         tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
         largura = int(tabuleiro[0])
         altura = int(tabuleiro[1])
 
-        #verificacao de dimensoes do tabuleiro
-        while largura > 10 or largura<=0 or altura > 10 or altura<=0:
+        #verificacao de dimencoes do tabuleiro
+        while largura > 10 or largura<4 or altura > 10 or altura<4:
             print('Medidas inválidas de tabuleiro, por favor digite novamente!')
             tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
         matrizGame = []
@@ -169,19 +212,89 @@ while jogarNovamente:
             #inicia-se as verificacoes
             if vez >= 7:
                 if vez % 2 ==0:
-                    if analiseLinhaColuna(1,matrizGame):
+                    if analiseLinhaColuna(1,matrizGame) or analiseDiag(1,matrizGame):
                         print('==== Jogador 1 venceu! ====')
                         break
                 else:
-                    if analiseLinhaColuna(2,matrizGame):
+                    if analiseLinhaColuna(2,matrizGame) or analiseDiag(2,matrizGame):
                         print('==== Jogador 2 venceu! ====')
                         break
             vez+=1
             espacosVazios-=1
         if espacosVazios == 0:
             print('Nenhum dos jogadores chegaram na vitória e os espaços acabaram! EMPATE!')
+
+
+
+#========================= JOGADOR X COMPUTADOR =====================#
     else:
-        xxx = 0    
+        tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
+        largura = int(tabuleiro[0])
+        altura = int(tabuleiro[1])
+
+        #verificacao de dimencoes do tabuleiro
+        while largura > 10 or largura<4 or altura > 10 or altura<4:
+            print('Medidas inválidas de tabuleiro, por favor digite novamente!')
+            tabuleiro = input('Qual o tamanho do tabuleiro que os jogadores vão utilizar?(limite de 10x10, digite os números de largura e altura respectivamente, separados por espaço): ').split(' ')
+
+        matrizGame = []
+        qtdLinhas = 0
+        #criacao do tabuleiro(matriz)
+        while qtdLinhas < altura:
+            qtdCol = 0
+            linha = []
+            while qtdCol < largura:
+                linha.append(0)
+                qtdCol+=1
+            matrizGame.append(linha)
+            qtdLinhas+=1
+
+
+        print('-'*20)    
+        print('Este é o tabuleiro da sua partida! Que vença o melhor! ')  
+        mostraMatriz(matrizGame)
+
+        # Decidindo qual jogador começa
+
+        print('Para decidir qual jogador irá iniciar faremos um sorteio... ')
+        sorteio = random.randint(-1,1)
+        time.sleep(3)
+        print('E o jogador que iniciará será...')
+        time.sleep(3)
+        if sorteio == 0:
+            print('* Jogador 1! Você começa e ficará com as peças de número 1!')
+            vez = 0
+        else:
+            print('* Jogador 2! Você começa e ficará com as peças de número 2!')
+            vez = 1
+
+        #iniciando o jogo
+        espacosVazios = altura * largura
+        while espacosVazios >= 0:
+            if vez%2==0:
+                jogada(1,matrizGame)
+            else:
+                jogada(3,matrizGame)
+            #condicao mínima para uma vitoria é de pelo menos 7 jogadas (4 do vencedor e 3 do perdedor)
+            #inicia-se as verificacoes
+            if vez >= 7:
+                if vez % 2 ==0:
+                    if analiseLinhaColuna(1,matrizGame) or analiseDiag(1,matrizGame):
+                        print('==== Jogador 1 venceu! ====')
+                        break
+                else:
+                    if analiseLinhaColuna(2,matrizGame) or analiseDiag(2,matrizGame):
+                        print('==== Jogador 2 venceu! ====')
+                        break
+            vez+=1
+            espacosVazios-=1
+        if espacosVazios == 0:
+            print('Nenhum dos jogadores chegaram na vitória e os espaços acabaram! EMPATE!')  
+
+
+
+            
+             
 
     #pergunta se o jogador deseja reiniciar o jogo                   
     jogarNovamente = input('Deseja jogar novamente?:\n 1 - SIM\n 2 - NAO\n ')
